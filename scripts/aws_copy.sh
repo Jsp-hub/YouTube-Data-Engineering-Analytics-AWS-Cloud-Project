40e7a927e092f5407ce73afa4e09987e1c5d52d6
Jeel Patel <jeel1410patel@gmail.com>
2026-07-26 23:37:25 -0400

    Completed Silver Layer

---
+++ scripts/aws_copy.sh
@@ -0,0 +1,39 @@
+# CA
+aws s3 cp CA_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=CA/
+aws s3 cp CAvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=CA/
+
+# DE
+aws s3 cp DE_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=DE/
+aws s3 cp DEvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=DE/
+
+# FR
+aws s3 cp FR_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=FR/
+aws s3 cp FRvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=FR/
+
+# GB
+aws s3 cp GB_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=GB/
+aws s3 cp GBvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=GB/
+
+# IN
+aws s3 cp IN_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=IN/
+aws s3 cp INvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=IN/
+
+# JP
+aws s3 cp JP_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=JP/
+aws s3 cp JPvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=JP/
+
+# KR
+aws s3 cp KR_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=KR/
+aws s3 cp KRvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=KR/
+
+# MX
+aws s3 cp MX_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=MX/
+aws s3 cp MXvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=MX/
+
+# RU
+aws s3 cp RU_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=RU/
+aws s3 cp RUvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=RU/
+
+# US
+aws s3 cp US_category_id.json s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=US/
+aws s3 cp USvideos.csv s3://yt-data-pipeline-bronze-us-east-1-devjeel/youtube/raw_statistics/region=US/
